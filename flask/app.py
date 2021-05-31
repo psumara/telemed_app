feab92f17e243f1f870301cde88d26293f11b8b8
mrkomal <gry2@vp.pl>
2021-05-31 23:52:14 +0200

    Getting best scores from DB with flask

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from api.database_operations import DBOperations
+
+app = Flask(__name__)
+db = DBOperations()
+
+
+@app.route('/best_scores.html')
+def best_scores():
+    best_scores_data = db.get_best_scores(5)
+    return render_template('/best_scores.html', data=best_scores_data)
+
+
+@app.route('/best_score.html')
+def best_score():
+    best_score_data = db.get_best_scores(1)
+    return render_template('/best_score.html', data=best_score_data)

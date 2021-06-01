7de55f3c4265db4b6ae9eee02e482bd76afb2cd9
mrkomal <gry2@vp.pl>
2021-06-01 15:10:38 +0200

    Function to insert score to database

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from api.database_operations import DBOperations
 
 app = Flask(__name__)
@@ -14,6 +14,13 @@ def best_scores():
         for score in best_scores_data:
             score['Score'] = str(score['Score'])
         return jsonify(best_scores_data)
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    current_score = request.json
+    if current_score is not None:
+        db.submit_score("anonymous_user", current_score)
 
 
 if __name__ == '__main__':
